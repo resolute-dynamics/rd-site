8b8b87af098907bba59b6cc605963c1c6788ecf5
Karthick <karthickk1996@hotmail.com>
2025-03-27 11:29:41 +0530

    Update home-page-mail.php

--- home-page-mail.php
+++ home-page-mail.php
@@ -5,47 +5,19 @@ use PHPMailer\PHPMailer\Exception;
 
 require_once "vendors/autoload.php";
 
-//PHPMailer Object
 $mail = new PHPMailer();
-$name=$_REQUEST['name'] ?? '';
-$email=$_REQUEST['email'] ?? '';
-$phone=$_REQUEST['phone'] ?? '';
-$subject=$_REQUEST['subject'] ?? '';
-   
-$message=$_REQUEST['message'] ?? '';
-
-//From email address and name
-$mail->From = "noreply@resolute-dynamics.com";
-$mail->FromName = "Resolute Dynamics";
-
-//To address and name
-$mail->addAddress("resolutedynamics07@gmail.com", "Resolute Dynamics");
-
-$mail->IsSMTP();
-$mail->Host = "smtp.zeptomail.in";
-
-
-
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission 465 ssl
-$mail->Port = 587;
-
-//Set the encryption system to use - ssl (deprecated) or tls
-$mail->SMTPSecure = 'tls';
-
-$mail->SMTPOptions = array(
-        'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-        )
-        );
-
-// optional
-// used only when SMTP requires authentication  
+$mail->Encoding = "base64";
 $mail->SMTPAuth = true;
-$mail->Username = 'emailapikey';
+$mail->Host = "smtp.zeptomail.in";
+$mail->Port = 587;
+$mail->Username = "emailapikey";
 $mail->Password = 'PHtE6r1YRei+g2V6p0MFtqTuFJL2MN4qq+sxLwEUtowWDKACG00E/dp/l2Wwr0orUfBDRaSanYxu5L+btenUdG/lPT5NDmqyqK3sx/VYSPOZsbq6x00etFUdcEzVUYXtcN9p1yDSst/eNA==';
-
+$mail->SMTPSecure = 'TLS';
+$mail->From = "noreply@resolute-dynamics.com";
+$mail->addAddress('karthickk1996@hotmail.com');
+$mail->isSMTP();
+$mail->IsHTML(true);
+$mail->CharSet = "UTF-8";
 $mail->Subject = "Get in touch - ".$name;
 $mail->Body = "<html>
                           <head>
@@ -75,7 +47,7 @@ try {
    // header('Location: thanks.php');
    // echo "Message has been sent successfully";
 } catch (Exception $e) {
-    echo true;
+    echo false;
    // header('Location: thanks.php');
    // echo "Mailer Error: " . $mail->ErrorInfo;
 }
